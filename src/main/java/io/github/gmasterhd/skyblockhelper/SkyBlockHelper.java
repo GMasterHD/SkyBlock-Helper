1bd519d3e44b7664862875c7fff5b61e4c41df83
GMasterHD <xxgmasterhdxx@gmail.com>
2020-12-19 00:10:12 +0100

    Initial Commit

---
+++ src/main/java/io/github/gmasterhd/skyblockhelper/SkyBlockHelper.java
@@ -0,0 +1,19 @@
+package io.github.gmasterhd.skyblockhelper;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid=SkyBlockHelper.MODID, version = SkyBlockHelper.VERSION, acceptedMinecraftVersions = "1.8.9", clientSideOnly = true)
+public class SkyBlockHelper {
+	public static final String MODID = "skyblockhelper";
+	public static final String VERSION = "0.0.1";
+	
+	public void preInit(FMLPreInitializationEvent e) {
+	}
+	public void init(FMLInitializationEvent e) {
+	}
+	public void postInit(FMLPostInitializationEvent e) {
+	}
+}
